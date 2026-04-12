68341b87075049ca4e91e1d51e977b4f0be6983e
OliveroJ16 <jose.olivero.uni@gmail.com>
2026-04-12 14:20:31 -0500

    feat: implement global exception handler and standardized api error response

--- src/main/java/com/olivero/auth/auth_service/controller/AuthController.java
+++ src/main/java/com/olivero/auth/auth_service/controller/AuthController.java
@@ -3,20 +3,21 @@ package com.olivero.auth.auth_service.controller;
 import com.olivero.auth.auth_service.dto.request.RegisterRequest;
 import com.olivero.auth.auth_service.dto.response.UserResponse;
 import com.olivero.auth.auth_service.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
 
-    @PostMapping
-    public ResponseEntity<UserResponse> registerUser(@RequestBody RegisterRequest registerRequest) {
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
         UserResponse response = userService.saveUser(registerRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
